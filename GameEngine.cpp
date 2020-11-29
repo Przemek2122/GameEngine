e7d49eb5c84d00ad5592a7e6b2285bbaa5284696
Przemek <przemek12wiewiora@gmail.com>
2020-11-29 14:54:37 +0100

    Update to working version

---
+++ GameEngine.cpp
@@ -0,0 +1,13 @@
+// GameEngine.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include <iostream>
+
+int StartGameEngine()
+{
+	std::cout << "Game engine here!" << std::endl;
+
+	return 1;
+}
