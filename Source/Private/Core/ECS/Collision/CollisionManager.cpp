1de8927d933fb9e5c2d6f01f1ddeb7278d58c2d5
Przemek <przemek12wiewiora@gmail.com>
2024-05-25 17:18:45 +0200

    Collision mesh in progress

--- Source/Private/Core/ECS/Collision/CollisionManager.cpp
+++ Source/Private/Core/ECS/Collision/CollisionManager.cpp
@@ -3,4 +3,66 @@
 #include "CoreEngine.h"
 #include "ECS/Collision/CollisionManager.h"
 
+#include "Renderer/Map/Map.h"
+#include "Renderer/Map/MapManager.h"
 
+FCollisionManager::FCollisionManager()
+{
+}
+
+void FCollisionManager::InitializeSubSystem()
+{
+	ISubSystemInstanceInterface::InitializeSubSystem();
+
+	FWindow* Window = dynamic_cast<FWindow*>(GetSubSystemParentInterface());
+	if (Window != nullptr)
+	{
+		FMapManager* MapManager = Window->GetMapManager();
+		if (MapManager != nullptr)
+		{
+			FMap* CurrentMap = MapManager->GetCurrentMap();
+			if (CurrentMap != nullptr)
+			{
+				FVector2D<int> TilesNumber = CurrentMap->GetMapSizeInTiles();
+
+				// Create mesh for collision
+			}
+		}
+	}
+}
+
+void FCollisionManager::TickSubSystem()
+{
+	Super::TickSubSystem();
+
+	CheckCircleCollision();
+	CheckSquareCollision();
+}
+
+void FCollisionManager::RegisterCircleCollision(FCircleCollision* InCircleCollision)
+{
+	CircleCollisionArray.Push(InCircleCollision);
+}
+
+void FCollisionManager::UnRegisterCircleCollision(FCircleCollision* InCircleCollision)
+{
+	CircleCollisionArray.Remove(InCircleCollision);
+}
+
+void FCollisionManager::RegisterSquareCollision(FSquareCollision* SquareCollision)
+{
+	SquareCollisionArray.Push(SquareCollision);
+}
+
+void FCollisionManager::UnRegisterSquareCollision(FSquareCollision* SquareCollision)
+{
+	SquareCollisionArray.Remove(SquareCollision);
+}
+
+void FCollisionManager::CheckCircleCollision()
+{
+}
+
+void FCollisionManager::CheckSquareCollision()
+{
+}
