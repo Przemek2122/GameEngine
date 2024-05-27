61ad7ba887286f0e3f32c97989df72afc2d2bbcb
Przemek <przemek12wiewiora@gmail.com>
2024-05-27 21:23:21 +0200

    Update Collision component

--- Source/Private/Core/ECS/Components/CollisionComponent.cpp
+++ Source/Private/Core/ECS/Components/CollisionComponent.cpp
@@ -1,5 +1,10 @@
 // Created by Przemys³aw Wiewióra 2024
 
 #include "CoreEngine.h"
+#include "ECS/Components/CollisionComponent.h"
 
 
+UCollisionComponent::UCollisionComponent(IComponentManagerInterface* InComponentManagerInterface)
+	: UBaseComponent(InComponentManagerInterface)
+{
+}
---
+++ Source/Public/Core/ECS/Components/CollisionComponent.h
@@ -0,0 +1,17 @@
+// Created by Przemys³aw Wiewióra 2020-2024
+
+#pragma once
+
+#include "ECS/BaseComponent.h"
+
+/**
+ * Component for collision management
+ */
+class UCollisionComponent : public UBaseComponent
+{
+public:
+	UCollisionComponent(IComponentManagerInterface* InComponentManagerInterface);
+
+
+
+};
