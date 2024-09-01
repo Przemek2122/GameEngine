1b8ae15e8e933eea6d40874fac09c976285c1279
Przemek <przemek12wiewiora@gmail.com>
2024-09-01 22:35:42 +0200

    Border widget class

---
+++ Source/Private/Renderer/Widgets/Samples/BorderWidget.cpp
@@ -0,0 +1,6 @@
+// Created by Przemys³aw Wiewióra 2024
+
+#include "CoreEngine.h"
+#include "Renderer/Widgets/Samples/Border.h"
+
+
---
+++ Source/Public/Renderer/Widgets/Samples/BorderWidget.h
@@ -0,0 +1,8 @@
+// Created by Przemys³aw Wiewióra 2020
+
+#pragma once
+
+class FBorderWidget : public FWidget
+{
+
+};
