616d44783321451a6277570bbd19463784f26f5d
Przemek <przemek12wiewiora@gmail.com>
2024-10-04 15:18:35 +0200

    Weapon base

--- Source/Private/Core/ECS/Entities/WeaponBase.cpp
+++ Source/Private/Core/ECS/Entities/WeaponBase.cpp
@@ -1,7 +1,61 @@
 #include "CoreEngine.h"
 #include "Core/ECS/Entities/WeaponBase.h"
 
+#include "Timer/TimerManager.h"
+
 EWeaponBase::EWeaponBase(FEntityManager* InEntityManager)
 	: EEntity(InEntityManager)
+	, bCanAttack(true)
+	, bIsDelayed(false)
+{
+}
+
+void EWeaponBase::Attack()
+{
+	if (CanAttack())
+	{
+		PerformAttack();
+
+		if (ShouldDelayAttacks())
+		{
+			FDelegateSafe<void, FOptionalTimerParams*> OnAttackDelayFinished;
+			OnAttackDelayFinished.BindObject(this, &EWeaponBase::AttackDelayFinished);
+
+			AttackDelayTimer = FTimerManager::CreateTimerSync(OnAttackDelayFinished, 0.1f);
+
+			bIsDelayed = true;
+		}
+
+		OnAttackPerformed();
+	}
+}
+
+void EWeaponBase::SetCanAttack(const bool bInCanAttack)
+{
+	bCanAttack = bInCanAttack;
+
+	OnCanAttackChanged();
+}
+
+void EWeaponBase::PerformAttack()
+{
+}
+
+void EWeaponBase::AttackDelayFinished(FOptionalTimerParams* OptionalTimerParams)
+{
+	OnAttackDelayFinished();
+
+	bIsDelayed = false;
+}
+
+void EWeaponBase::OnCanAttackChanged()
+{
+}
+
+void EWeaponBase::OnAttackPerformed()
+{
+}
+
+void EWeaponBase::OnAttackDelayFinished()
 {
 }
--- Source/Public/Core/ECS/Entities/WeaponBase.h
+++ Source/Public/Core/ECS/Entities/WeaponBase.h
@@ -2,9 +2,40 @@
 
 #include "CoreMinimal.h"
 
+struct FOptionalTimerParams;
+
+/**
+ * Base class for weapons with some basic functionality
+ */
 class EWeaponBase : public EEntity
 {
 public:
 	EWeaponBase(FEntityManager* InEntityManager);
+
+	/** Try to attack if possible */
+	virtual void Attack();
+
+	/** Defines if attack can be performed - Check for delay between attacks etc */
+	virtual bool CanAttack() const { return (bCanAttack && !bIsDelayed); }
+
+	virtual bool ShouldDelayAttacks() const { return true; }
+
+	/** Sets bool which disables or enables attacks */
+	void SetCanAttack(const bool bInCanAttack);
+
+protected:
+	virtual void PerformAttack();
+	void AttackDelayFinished(FOptionalTimerParams* OptionalTimerParams);
+
+	virtual void OnCanAttackChanged();
+	virtual void OnAttackPerformed();
+	virtual void OnAttackDelayFinished();
+
+private:
+	bool bCanAttack;
+	bool bIsDelayed;
+
+	/** Timer for delayed attacks */
+	std::shared_ptr<FTimer> AttackDelayTimer;
 
 };
